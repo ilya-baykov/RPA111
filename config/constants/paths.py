bfcb193329cdb25c3af9ef4e2afe1f7c27a6bfb6
ilyab <i-baykov@internet.ru>
2025-01-16 13:23:56 +0300

    добавил путь до env файла с информацией о бд

--- config/constants/paths.py
+++ config/constants/paths.py
@@ -1,5 +1,8 @@
 # Путь к каталогу env файлов
 ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\emv_files"
 
+# Путь к env файлу с информацией о БД
+DB_ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\emv_files\.db.env"
+
 # Путь к каталогу для сохранения файлов из таблицы
 SAVE_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\saved_files"
