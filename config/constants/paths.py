b5de96de7de442aa55e0d45a6d124fbf34abbf4b
ilyab <i-baykov@internet.ru>
2025-01-16 18:37:27 +0300

    Обновил пути

--- config/constants/paths.py
+++ config/constants/paths.py
@@ -1,8 +1,8 @@
 # Путь к каталогу env файлов
-ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\emv_files"
+ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111_fix\config\emv_files"
 
 # Путь к env файлу с информацией о БД
-DB_ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\emv_files\.db.env"
+DB_ENV_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111_fix\config\emv_files\.db.env"
 
 # Путь к каталогу для сохранения файлов из таблицы
-SAVE_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111\config\saved_files"
+SAVE_PATH = r"C:\Users\ilyab\PycharmProjects\RPA111_fix\config\saved_files"
