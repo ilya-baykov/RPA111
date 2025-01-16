6c397e0f396f1eab1725a8a11bbd7ee2bdc782a0
ilyab <i-baykov@internet.ru>
2025-01-16 13:29:52 +0300

    Изменил структуру папок

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import config.env_loader  # Необходимо для загрузки всех переменных окружения
+import pandas as pd
 
 from src.get_table_info.ossdep_requests import DataDownloader
+from src.get_table_info.dataframe_reader import DataFrameReader
 
 
 def main() -> None:
-    DataDownloader.download_data(params={'table': 'cons_dm.wfm_ppr_outsherpa'},
-                                 output_file_name='cons_dm.wfm_ppr_outsherpa.json.gzip')
+    file_path = DataDownloader.download_data(params={'table': 'cons_dm.wfm_ppr_outsherpa'},
+                                             output_file_name='showcase.txt')
+    data_frame: pd.DataFrame = DataFrameReader(file_path=file_path).get_file_dataframe()
+    print(data_frame)
 
 
 if __name__ == '__main__':
